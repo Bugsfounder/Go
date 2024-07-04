5e6e8613bc5500202495106b3822183b2c1b1cf0
BugsFounder <bugsfounder2021@gmail.com>
2024-07-04 21:43:37 +0530

    basics of go

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,41 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// return a greeting that embeds the name in a message
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	message := fmt.Sprintf(randomFormate(), name)
+	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	// loop through the received slice of names calling the Hello function to get a message for each name.
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// in the map, associate the retrieved message with the name.
+		messages[name] = message
+	}
+	return messages, nil
+
+}
+func randomFormate() string {
+	strMessage := []string{
+		"Hi, %v Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return strMessage[rand.Intn(len(strMessage))]
+}
--- hello/main.go
+++ hello/main.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
-import "go.com/greetings"
+import (
+	"fmt"
+	"log"
+
+	"go.com/greetings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-	message:= greetings. Hello("Bugs")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// slice of names.
+	names := []string{"Bugs", "Samantha", "Kosu"}
+
+	// request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal("err")
+
+	}
+
+	// if no error was returned, print the returned message to the console
+	fmt.Println(messages)
 }
